f85347fd3b340e51832de19e15931055bbda5d23
aleks <aleks@192.168.0.14>
2022-01-20 15:43:01 +0200

    Initial commit

---
+++ src/main/java/testimain1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author aleks
+ */
+public class testimain1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Moroo!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Paina entteriä äiä!");
+        scan.nextLine();
+        // TODO code application logic here
+    }
+    
+}
